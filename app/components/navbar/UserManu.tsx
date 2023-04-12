c2c558e46c5c164897c8dc8c41ef5536b3b3aef1
David Castellanos <davcastellanoslarios@gmail.com>
2023-04-12 00:15:34 -0600

    working into <user menu/>

--- app/components/navbar/UserManu.tsx
+++ app/components/navbar/UserManu.tsx
@@ -2,8 +2,17 @@
 
 import { AiOutlineMenu } from 'react-icons/ai'
 import Avatar from '../Avatar';
+import { useCallback, useState } from 'react';
 
 const UserManu = () => {
+
+    const [isOpen, setIsOpen] = useState(false)
+
+    const toggleOpen = useCallback(()=>{
+        setIsOpen( value => !value )
+    },[])
+
+
   return (
     <div className="relative">
         <div className="flex flex-row items-center gap-3">
@@ -14,7 +23,7 @@ const UserManu = () => {
             >
                 Airbnb your home
             </div>
-            <div onClick={()=>{}}
+            <div onClick= { toggleOpen }
                 className="p-4 md:py-1 md:px-2 border:-[1px]
                             border-neutral-200 flex flex-row
                             items-center gap-3 rounded-full cursor-pointer
@@ -26,6 +35,21 @@ const UserManu = () => {
                 </div>
             </div>
         </div>
+        {
+            isOpen && (
+                <div className="absolute rounded-xl shadow-md
+                                w-[40vw] md:w-3/4 bg-white overflow-hidden
+                                rigth-0 top-12 text-sm"
+                >
+                    <div className="flex flex-col cursor-pointer"
+                    >
+                        <>
+                            Menu Item Component
+                        </>
+                    </div>
+                </div>
+            )
+        }
     </div>
   )
 }
