75921865a604d1bd9dd0eb69fd37460d50a612fa
David Castellanos <davcastellanoslarios@gmail.com>
2023-04-11 22:49:01 -0600

    working into Search bar

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Container from '../Container';
 import Logo from './Logo';
+import Search from './Search';
 
 export default function Navbar() {
   return (
@@ -9,7 +10,7 @@ export default function Navbar() {
             <Container>
                 <div className="flex flex-row itemscenter justify-between gap-3 md:gap-0">
                     <Logo />
-                    
+                    <Search />
                 </div>
             </Container>
         </div>
