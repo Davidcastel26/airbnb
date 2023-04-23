d9473cf317fef28a3d0f9043cacea48d081ca095
David Castellanos <davcastellanoslarios@gmail.com>
2023-04-23 11:25:56 -0600

    working on logo.tsx

--- app/components/navbar/Logo.tsx
+++ app/components/navbar/Logo.tsx
@@ -4,10 +4,11 @@ import Image from 'next/image'
 import { useRouter } from 'next/navigation';
 
 const Logo = () => {
-    const router = useRouter()
+  
+  const router = useRouter()
 
   return (
-    <Image alt='Logo' className='hidden md:block cursor-pointer' height='100' width='100' src='/images/logo.png'/>
+    <Image alt='Logo' className='hidden md:block cursor-pointer' width='100' height='100'  src='/images/logo.png'/>
   )
 }
 
